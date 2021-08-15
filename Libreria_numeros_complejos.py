b653f8ae6f24da345dc32ca896b0583b72339d55
Santiago Forero Yate <santiago.forero-y@mail.escuelaing.edu.co>
2021-08-14 20:52:35 -0500

    Libreria_numeros_complejos

---
+++ Libreria_numeros_complejos.py
@@ -0,0 +1,123 @@
+"""Libreria de operaciones basicas con numeros complejos
+
+"""
+
+import math
+
+
+def rep_cartesiana(par):
+    """representa un numero complejo en representanción cartesiana
+    
+    :param par: representacion polar de un numero complejo
+    :type par: list
+    :return: representación cartesiana de un nnumero complejo
+    :rtype: list
+    """
+    complejo = [par[0] * math.cos(par[1]), par[0] * math.sin(par[1])]
+
+    return complejo
+
+
+def rep_polar(par):
+    """representa un numero complejo en una representacion polar
+
+    :param par: representacion cartesiana de un numero complejo
+    :type par: list
+    :return: representacion polar de un numero complejo
+    :rtype: list
+    """
+    p = modulo_complejos(par)
+    ang = math.atan(par[1] / par[0])
+    result = [p, ang]
+
+    return result
+
+
+def conjugado_complejos(par):
+    """ realiza el conjugado de un numero complejo
+
+    :param par: representacion de un numero complejo en coordenadas cartesianas
+    :type par: list
+    :return: el numero complejo conjugado
+    :rtype: list
+    """
+    new_b = par[1] * -1
+    new_par = [par[0], new_b]
+
+    return new_par
+
+
+def modulo_complejos(par):
+    """ Realiza el conjugado de un numero complejo
+
+    :param par: representación de un numero complejo en coordenadas cartesianas
+    :type par: list
+    :return: un entero valor del modulo de un numero complejo
+    :rtype: int
+    """
+    modulo = (par[0] ** 2 + par[1] ** 2)
+
+    return math.sqrt(modulo)
+
+
+def division_complejos(par1, par2):
+    """
+
+    :param par1: representación de un numero complejo en coordenadas cartesianas
+    :type par1: list
+    :param par2: representación de un numero complejo en coordenadas cartesianas
+    :type par2: list
+    :return: una lista como representación de el numero complejo resultante de la division
+    :rtype: list
+    """
+    d = (par2[0] ** 2 + par2[1] ** 2)
+    x = (par1[0] * par2[0] + par1[1] * par2[1]) / d
+    y = (par2[0] * par1[1] - par1[0] * par2[1]) / d
+    result = [x, y]
+
+    return result
+
+
+def resta_complejos(par1, par2):
+    """Resta de dos numeros complejos
+
+    :param par1: representación de un numero complejo en coordenadas cartesianas
+    :type par1: list
+    :param par2: representación de un numero complejo en coordenadas cartesianas
+    :type par2: list
+    :return: una lista como representación de el numero complejo resultante de la resta
+    :rtype: list
+    """
+    result = [par1[0] - par2[0], par1[1] - par2[1]]
+
+    return result
+
+
+def suma_complejos(par1, par2):
+    """Suma dos numeros complejos
+
+    :param par1: representación de un numero complejo en coordenadas cartesianas
+    :type par1: list
+    :param par2: representación de un numero complejo en coordenadas cartesianas
+    :type par2: list
+    :return: una lista como representación de el numero complejo resultante de la suma
+    :rtype: list
+    """
+    result = [par1[0] + par2[0], par1[1] + par2[1]]
+
+    return result
+
+
+def multiplicacion_complejos(par1, par2):
+    """Multiplicacion de dos numeros complejos
+
+    :param par1: representación de un numero complejo en coordenadas cartesianas
+    :type par1: list
+    :param par2: representación de un numero complejo en coordenadas cartesianas
+    :type par2: list
+    :return: una lista como representación de el numero complejo resultante de la multiplicacion
+    :rtype: list
+    """
+    result = [(par1[0] * par2[0]) - (par1[1] * par2[1]), (par1[0] * par2[1]) + (par1[1] * par2[0])]
+
+    return result
